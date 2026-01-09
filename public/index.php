6b40b5039ffc767d395636f4fb02f7c6bd83ad86
DesaleRohit <rohitdesale2005@gmail.com>
2026-01-09 18:47:34 +0530

    [ADDED]:Rohit - Project Setup

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>RO Water Delivery System</title>
+</head>
+
+<body>
+
+    <h1>Welcome to RO Water Delivery Service</h1>
+    <p>Clean & safe drinking water at your doorstep.</p>
+
+    <a href="order.php">Order Water Can</a>
+
+</body>
+
+</html>
